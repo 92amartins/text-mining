349a0ffcdacb82f63c2017d8bf96f0775ae8fce4
Andrei Martins <andrei.martins@esx.com.br>
2017-09-15 19:32:36 -0300

    Enhance preprocessing

--- clean/create_bow.R
+++ clean/create_bow.R
@@ -1,9 +1,9 @@
 library(quanteda)
 
-create_bow <- function(crps){
+create_bow <- function(corpus){
     # Takes a corpus and returns a bag of words
-    bow <- dfm(corpus(crps),
-               tolower = TRUE,
-               stem = FALSE,
+    bow <- dfm(corpus(corpus),
+               tolower = TRUE, remove_numbers = TRUE, remove_punct = TRUE, 
+               remove_symbols = TRUE, remove_twitter = TRUE,
                remove = stopwords())
 }
